6dd9fccd0627269883ba580d4b8795ce371ec990
DennisLudmann <dennisludmann@gmail.com>
2022-03-07 12:13:17 +0100

    typo @ draw clearned, and character plus enemies drawn

--- models/world.class.js
+++ models/world.class.js
@@ -7,8 +7,29 @@ enemies = [
      new Chicken(),
     ];
 
-draw() {
-
+ canvas;
+ ctx;
+    
+constructor(canvas){
+    this.ctx = canvas.getContext('2d');
+    this.canvas = canvas;                   // to pass the details to the canvas outside the function
+    this.draw();
 }
 
+
+draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.hight);
+    this.enemies.forEach(enemy => {
+        this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.hight);
+    });
+
+
+    // Draw will be called repeatetly
+    let self = this;            // it doesnt accept this. in the function so you call this something else and parse in that something else
+    requestAnimationFrame(function(){
+        self.draw();
+    
+    });
+}
 }
