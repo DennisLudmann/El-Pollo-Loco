0242eb2961b46679e9fd2fc504dd83464ff3c97d
DennisLudmann <dennisludmann@gmail.com>
2022-03-08 11:51:42 +0100

    sice and position  of character and enemies

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,9 @@
 class Character extends MovableObject {
 
+    x = 50;
+    y = 133;
+    hight = 310;
+    width = 100;
     constructor(){
         super().loadImage('img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png');
     }
